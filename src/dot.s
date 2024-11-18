83bd4fa642c1fe269977c60d92c1ba998dbad018
wu81177 <wu81177@gmail.com>
2024-11-18 22:58:45 +0800

    Complete dot.s

--- src/dot.s
+++ src/dot.s
@@ -37,7 +37,41 @@ dot:
 loop_start:
     bge t1, a2, loop_end
     # TODO: Add your own implementation
+    # Load elements from arr0 and arr1
+    mv a5, t1
+    mv a6, a3 
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal start_mul
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    slli a5, a5, 2 
+    add a5, a5, a0       
+    lw t4, 0(a5)              # arr0[i * stride0]
 
+    mv a5, t1
+    mv a6, a4 
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal start_mul
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    slli a5, a5, 2 
+    add a5, a5, a1       
+    lw t5, 0(a5)               # arr1[i * stride1]
+
+    mv a5, t4
+    mv a6, t5
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal start_mul
+    lw ra, 0(sp)
+    addi sp, sp, 4
+
+    # return
+    add t0, t0, a5
+    addi t1, t1, 1
+    j loop_start
 
 loop_end:
     mv a0, t0
@@ -51,3 +85,34 @@ error_terminate:
 set_error_36:
     li a0, 36
     j exit
+
+# multiplier (a5*a6; return a5)
+start_mul:
+    xor t6, a5, a6            # mul sign
+    srli t6, t6, 31
+    li t3, 0
+
+    bge a5, zero, skip_0abs
+    sub a5, zero, a5
+skip_0abs:
+    bge a6, zero, mul_loop
+    sub a6, zero, a6
+
+mul_loop:
+    beq a6, zero, mul_end
+    andi t2, a6, 1
+    beq t2, zero, skip_accu
+
+    add t3, t3, a5
+
+skip_accu:
+    slli a5, a5, 1
+    srli a6, a6, 1
+    j mul_loop
+
+mul_end:
+    beq t6, zero, positive
+    sub t3, zero, t3
+positive:
+    add a5, zero, t3
+    jr ra
