5befa18023e9ac25786268110162633f56c2dce5
wu81177 <wu81177@gmail.com>
2024-11-18 19:30:12 +0800

    Complete relu.s

--- src/relu.s
+++ src/relu.s
@@ -29,7 +29,21 @@ relu:
 
 loop_start:
     # TODO: Add your own implementation
+    bge t1, a1, loop_end
+    slli t3, t1, 2
+    add t3, t3, a0
+    lw t4, 0(t3)
+    blt t4, zero, turn0
+    addi t1, t1, 1
+    j loop_start
+turn0:
+    sw zero, 0(t3)
+    addi t1, t1, 1
+    j loop_start
 
+loop_end:
+    addi a0, t1, 0 
+    jr ra
 error:
     li a0, 36          
     j exit          
